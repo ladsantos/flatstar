0069a07eb5bfa859734fcecfa988e991e9bfeee0
leonardo <leonardo.dossantos@unige.ch>
2021-05-15 10:11:12 +0200

    Added tests and improved `README.md`

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # flatstar
-Make 2-maps of limb-darkened stars.
+
+`flatstar` is a pure-Python tool for drawing stellar disks with scientifically-accurate limb darkening. Each pixel has an accurate fractional intensity in relation to the total stellar intensity of `1.0`. It is ideal for ray-tracing simulations of stars and planetary transits.
+
+![flatstar](assets/flatstar.png)
+
+## Features
+
+* Blazing fast! There are no for-loops involved, only `numpy` arrays manipulation and `pillow` Image objects.
+* It has the most well-known limb-darkening laws: linear, quadratic, square-root, logarithmic, exponential, Sing et al. (2009), Claret et al. (2000).
+* You can implement your own custom limb-darkening law.
+* Supersampling for the situations where you need coarse arrays but do not want to lose precision in stellar disk intensity (i.e., no hard pixel boundaries).
+* Resampling is done with the C-libraries of `pillow`, which means they are as fast as it goes.
+
+## Installation
+
+For now there is only one way to install `flatstar`: compiling from source.
+
+### Compile from source (development version)
+
+First clone this repository and navigate to it:
+
+```angular2html
+git clone https://github.com/ladsantos/flatstar.git && cd flatstar
+```
+
+And now install it:
+
+```angular2html
+python setup.py install
+```
---
+++ tests/test_draw.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from flatstar import draw
+
+
+IMPLEMENTED_LD_LAWS = ["linear", "quadratic", "square-root", "log", "exp",
+                       "sing", "claret"]
+N_LAWS = len(IMPLEMENTED_LD_LAWS)
+TEST_COEFFICIENTS = [np.random.random(),
+                     np.random.random(size=2),
+                     np.random.random(size=2),
+                     np.random.random(size=2),
+                     np.random.random(size=2),
+                     np.random.random(size=3),
+                     np.random.random(size=4)]
+
+IMPLEMENTED_SAMPLERS = ["nearest", "box", "bilinear", "hamming", "bicubic",
+                        "lanczos"]
+N_SAMPLERS = len(IMPLEMENTED_SAMPLERS)
+
+
+# Test each limb-darkening law
+def test_ld_laws(grid_size=101):
+    did_it_work = 1
+    exception_in = []
+    for i in range(N_LAWS):
+        try:
+            star = draw.star(grid_size,
+                             limb_darkening_law=IMPLEMENTED_LD_LAWS[i],
+                             ld_coefficient=TEST_COEFFICIENTS[i])
+            did_it_work *= 1
+        except:
+            did_it_work *= 0
+            exception_in.append(IMPLEMENTED_LD_LAWS[i])
+
+    assert(did_it_work == 1)
+
+
+# Test the supersampling and the resampling (aka downsampling)
+def test_supersampling(grid_size=100, factor=np.random.random() * 10, use_ld=6):
+    did_it_work = 1
+    exception_in = []
+    for i in range(N_SAMPLERS):
+        try:
+            star = draw.star(grid_size,
+                             limb_darkening_law=IMPLEMENTED_LD_LAWS[use_ld],
+                             ld_coefficient=TEST_COEFFICIENTS[use_ld],
+                             supersampling=factor,
+                             resample_method=IMPLEMENTED_SAMPLERS[i])
+            did_it_work *= 1
+        except:
+            did_it_work *= 0
+            exception_in.append(IMPLEMENTED_SAMPLERS[i])
+
+    assert(did_it_work == 1)
