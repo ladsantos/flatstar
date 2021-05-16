Several bug fixes in calculation of limb darkening and testing, implemented `utils.py`

flatstar/draw.py
@@ -7,11 +7,11 @@ limb darkening.
 
 import numpy as np
 import warnings
-from flatstar import limb_darkening
+from flatstar import limb_darkening, utils
 
 from PIL import Image, ImageDraw
 
-__all__ = ["star", "planet"]
+__all__ = ["star", "planet_transit"]
 
 
 IMPLEMENTED_LD_LAW = {"linear": limb_darkening.linear,
@@ -124,10 +124,9 @@ def star(grid_size, radius=0.5, limb_darkening_law=None, ld_coefficient=None,
                        shape=shape)
 
     # We need to know what is the distance of each pixel from the stellar center
-    # There is a little bit of hack in here to avoid using for-loops
-    coords = np.linspace(-center, center, effective_grid_size)
-    coords_x, coords_y = np.meshgrid(coords, coords)
-    r_array = (coords_x ** 2 + coords_y ** 2) ** 0.5
+    # There is a useful function in ``utils`` for that, and it does not use
+    # for-loops
+    r_array = utils.cylindrical_r(star_array)
 
     # Now we calculate the mu for the limb-darkening law
     # We ignore a RuntimeWarning here because any NaN will be multiplied by zero
@@ -153,20 +152,13 @@ def star(grid_size, radius=0.5, limb_darkening_law=None, ld_coefficient=None,
             raise NotImplementedError("This limb-darkening law is not "
                                       "implemented.")
 
-    # Calculate the normalization factor
-    # norm = np.sum(star_array)  # Normalization factor is the total intensity
-
     # We use PIL.Image to perform the resizing
     im = Image.fromarray(star_array)
     final_shape = (grid_size, grid_size)
 
-    # Downsample the supersampled array to the desired grid size if necessary
+    # Resize the array to the desired grid size if necessary
     if supersampling is not None or upscaling is not None:
         pass
-    #     rescaled_norm = norm / supersampling ** 2
-    # # Or upscale the array
-    # elif upscaling is not None:
-    #     rescaled_norm = norm * upscaling ** 2
     else:  # No resizing needed
         norm = np.sum(star_array)
         grid = star_array / norm  # Add star to the grid
@@ -183,6 +175,7 @@ def star(grid_size, radius=0.5, limb_darkening_law=None, ld_coefficient=None,
                                       "implemented.")
     # If the resample_method is not defined, then simply use a box interpolation
     else:
+        resample_method = 'box'
         final_star_array = im.resize(final_shape, resample=Image.BOX)
     # Finally make `star_array` as a copy of the downsampled array
     star_array = np.copy(final_star_array)
@@ -190,11 +183,17 @@ def star(grid_size, radius=0.5, limb_darkening_law=None, ld_coefficient=None,
     # Adding the star to the grid
     norm = np.sum(star_array)
     grid = star_array / norm
+
+    # grid = objects.StarGrid(intensity_array, radius, limb_darkening_law,
+    #                         ld_coefficient, supersampling, upscaling,
+    #                         resample_method)
+
     return grid
 
 
-# Why not draw a planet?
-def planet():
+# Draw a transit on a star
+def planet_transit(star_grid, planet_to_star_ratio, impact_parameter=0.0,
+                   phase=0.0):
     raise NotImplementedError("Drawing a planet is not implemented yet.")
 
 

flatstar/limb_darkening.py
@@ -127,6 +127,8 @@ def logarithmic(mu, c, i0=1.0):
     """
     c1, c2 = c
     attenuation = 1 - c1 * (1 - mu) - c2 * mu * np.log(mu) ** 2
+    # Remove the NaNs
+    attenuation[np.isnan(attenuation)] = 0.0
     i_mu = i0 * attenuation
     return i_mu
 
@@ -156,7 +158,12 @@ def exponential(mu, c, i0=1.0):
         ``mu``.
     """
     c1, c2 = c
-    attenuation = 1 - c1 * (1 - mu) - c2 / (1 - np.exp(mu))
+    # This particular limb-darkening law requires some sleight of hand to avoid
+    # numerical exceptions and warnings
+    term1 = np.copy(c1 * (1 - mu))
+    mu[mu <1E-16] = -np.inf
+    term2 = c2 / (1 - np.exp(mu))
+    attenuation = 1 - term1 - term2
     i_mu = i0 * attenuation
     return i_mu
 

tests/test_draw.py
@@ -28,16 +28,12 @@ def test_ld_laws(grid_size=101):
     did_it_work = 1
     exception_in = []
     for i in range(N_LAWS):
-        try:
-            star = draw.star(grid_size,
-                             limb_darkening_law=IMPLEMENTED_LD_LAWS[i],
-                             ld_coefficient=TEST_COEFFICIENTS[i])
-            did_it_work *= 1
-        except:
-            did_it_work *= 0
-            exception_in.append(IMPLEMENTED_LD_LAWS[i])
-
-    assert(did_it_work == 1)
+        star = draw.star(grid_size,
+                         limb_darkening_law=IMPLEMENTED_LD_LAWS[i],
+                         ld_coefficient=TEST_COEFFICIENTS[i])
+        total_intensity = np.sum(star)
+        obtained_precision = abs(1.0 - total_intensity)
+        assert (obtained_precision < REQUIRED_INTENSITY_PRECISION)
 
 
 # Test a custom limb-darkening law
@@ -72,18 +68,14 @@ def test_supersampling(grid_size=100, factor=np.random.randint(2, 10), use_ld=6)
     did_it_work = 1
     exception_in = []
     for i in range(N_SAMPLERS):
-        try:
-            star = draw.star(grid_size,
-                             limb_darkening_law=IMPLEMENTED_LD_LAWS[use_ld],
-                             ld_coefficient=TEST_COEFFICIENTS[use_ld],
-                             supersampling=factor,
-                             resample_method=IMPLEMENTED_SAMPLERS[i])
-            did_it_work *= 1
-        except:
-            did_it_work *= 0
-            exception_in.append(IMPLEMENTED_SAMPLERS[i])
-
-    assert(did_it_work == 1)
+        star = draw.star(grid_size,
+                         limb_darkening_law=IMPLEMENTED_LD_LAWS[use_ld],
+                         ld_coefficient=TEST_COEFFICIENTS[use_ld],
+                         supersampling=factor,
+                         resample_method=IMPLEMENTED_SAMPLERS[i])
+        total_intensity = np.sum(star)
+        obtained_precision = abs(1.0 - total_intensity)
+        assert (obtained_precision < REQUIRED_INTENSITY_PRECISION)
 
 
 # Test the upscaling
@@ -91,15 +83,11 @@ def test_upscaling(grid_size=500, factor=np.random.random() * 10, use_ld=6):
     did_it_work = 1
     exception_in = []
     for i in range(N_SAMPLERS):
-        try:
-            star = draw.star(grid_size,
-                             limb_darkening_law=IMPLEMENTED_LD_LAWS[use_ld],
-                             ld_coefficient=TEST_COEFFICIENTS[use_ld],
-                             upscaling=factor,
-                             resample_method=IMPLEMENTED_SAMPLERS[i])
-            did_it_work *= 1
-        except:
-            did_it_work *= 0
-            exception_in.append(IMPLEMENTED_SAMPLERS[i])
-
-    assert(did_it_work == 1)
+        star = draw.star(grid_size,
+                         limb_darkening_law=IMPLEMENTED_LD_LAWS[use_ld],
+                         ld_coefficient=TEST_COEFFICIENTS[use_ld],
+                         upscaling=factor,
+                         resample_method=IMPLEMENTED_SAMPLERS[i])
+        total_intensity = np.sum(star)
+        obtained_precision = abs(1.0 - total_intensity)
+        assert (obtained_precision < REQUIRED_INTENSITY_PRECISION)